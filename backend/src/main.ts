6b4e25b34d45b60f7f115011e924c9bd1e1cf2e1
Lellundril <GuillaumeLetzelter@gmail.com>
2023-10-25 11:17:36 +0200

    Added and activated Logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
 import { 
   FastifyAdapter, 
@@ -7,8 +8,12 @@ import {
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule, 
+    new FastifyAdapter({ logger: true })
+    );
   await app.listen(3000);
+  Logger.log('Application served at http://localhost:3000', 'bootstrap');
 }
 
 bootstrap();
