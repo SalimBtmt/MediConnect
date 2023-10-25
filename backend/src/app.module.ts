996619ac152bb8609f35e667f931478bf063e585
Lellundril <GuillaumeLetzelter@gmail.com>
2023-10-25 11:57:35 +0200

    Added Mongoose module and MongoDB config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoctorModule } from './doctor/doctor.module';
+import * as Config from 'config';
 
 @Module({
-  imports: [DoctorModule],
+  imports: [
+    DoctorModule,
+    MongooseModule.forRoot(Config.get<string>('mongodb.uri'))
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
