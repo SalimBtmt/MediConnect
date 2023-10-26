087e1dcca922e16b039382485094b657765be97f
salim <sboutament@gmail.com>
2023-10-26 17:25:59 +0200

    Adding the navigation - Creating components body, home, patient, consultations and sidenav - Updating the app-rooting-module - Adding some packages

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {}
