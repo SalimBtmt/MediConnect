be436b33172d2a17e92a9507a3f7dc45cf27f816
salim <sboutament@gmail.com>
2023-10-30 22:34:10 +0100

    Adding patient data to home page (Actually there is data for all doctors, not a specific one)

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,53 +1,60 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import {LiveAnnouncer} from '@angular/cdk/a11y';
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Patient } from '../shared/types/patient.type';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  
 })
-export class HomeComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class HomeComponent implements OnInit, AfterViewInit {
+  private _patient: Patient[] = [];
+  private readonly _backendURL: any;
 
-  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+  displayedColumns: string[] = ['firstname', 'lastname', 'birthDate', 'bloodtype'];
+  dataSource = new MatTableDataSource<Patient>(); // Change the type to Patient
 
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
+
+  constructor(private _http: HttpClient, private _liveAnnouncer: LiveAnnouncer) {
+    this._backendURL = {};
+
+    let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
+    if (environment.backend.port) {
+      baseUrl += `:${environment.backend.port}`;
+    }
+    // build all backend urls
+    // @ts-ignore
+    Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[ k ] = `${baseUrl}${environment.backend.endpoints[ k ]}`);
+  }
+
+  ngOnInit(): void {
+    this._http
+      .get<Patient[]>(this._backendURL.allPatients)
+      .subscribe({
+        next: (patients: Patient[]) => {
+          this._patient = patients;
+          
+          this.dataSource.data = this._patient; // Set patient data in MatTable
+          console.log(this.dataSource.data)
+        },
+        error: (error) => {
+          console.error('Error fetching patient data', error);
+        },
+      });
+
+      
+  }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
 
-  /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
