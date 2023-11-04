f6d07bb729bdcc38ab11d77527c129818852c818
salim <sboutament@gmail.com>
2023-11-04 02:03:57 +0100

    Frontend - Updating Home Component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -5,6 +5,14 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Patient } from '../shared/types/patient.type';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Router, NavigationEnd } from '@angular/router';
+
+
+// Interface for the sidenav toggle event
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-home',
@@ -12,6 +20,18 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+
+
+  isSignInOrSignUpRoute: boolean = false;
+
+  title = 'frontend';
+
+  // Flag to track the collapse state of the sidenav
+  isSideNavCollappsed: boolean = false;
+  // Variable to store the current screen width
+  screenWidth = 0;
+
+
   private _patients: Patient[] = [];
   private readonly _backendURL: any;
 
@@ -20,7 +40,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
 
-  constructor(private _http: HttpClient, private _liveAnnouncer: LiveAnnouncer) {
+  constructor(private _http: HttpClient, private _liveAnnouncer: LiveAnnouncer, private router: Router) {
     this._backendURL = {};
 
     let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
@@ -30,6 +50,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
     // build all backend urls
     // @ts-ignore
     Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[ k ] = `${baseUrl}${environment.backend.endpoints[ k ]}`);
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current route is /signin or /signup
+        this.isSignInOrSignUpRoute = ['/signin', '/signup'].includes(event.url);
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -45,6 +72,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
           console.error('Error fetching patient data', error);
         },
       });
+  }
+
+
+  onToggleSideNav(data: SideNavToggle): void {
+    // Update the collapse state and screen width based on the event data
+    this.isSideNavCollappsed = data.collapsed;
+    this.screenWidth = data.screenWidth;
   }
 
   ngAfterViewInit() {
@@ -77,5 +111,24 @@ export class HomeComponent implements OnInit, AfterViewInit {
           console.error('Error deleting patient', error);
         },
       });
+  }
+
+  // Method to determine the CSS class for the body element
+  getBodyClass(): string {
+    let styleClass = '';
+    
+    // If the sidenav is collapsed and the screen width is greater than 768 pixels, set the style class to 'body-trimmed'
+    if(this.isSideNavCollappsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed';
+    
+      // If the sidenav is collapsed and the screen width is less than or equal to 768 pixels and greater than 0, set the style class to 'body-md-screen'
+    } else if(this.isSideNavCollappsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+      styleClass = 'body-md-screen'
+    } 
+
+    if (this.isSignInOrSignUpRoute){
+      styleClass = "auth-page"
+    }
+    return styleClass;
   }
 }
