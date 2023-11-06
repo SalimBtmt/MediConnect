461a12a8da591b7424e01ff02d1c3798c197b5d6
salim <sboutament@gmail.com>
2023-11-06 01:53:35 +0100

    Adding AuthGuards

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,8 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guards';
 import { JwtStrategy } from './jwt.strategy';
 import { DoctorSchema } from './../doctor/schemas/doctor.schema';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
   imports: [
@@ -20,7 +23,10 @@ import { DoctorSchema } from './../doctor/schemas/doctor.schema';
     MongooseModule.forFeature([{ name: 'Doctor', schema: DoctorSchema }]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy,{
+    provide: APP_GUARD,
+    useClass: AuthGuard,
+  },],
   exports: [JwtStrategy,PassportModule],
 })
 export class AuthModule {}
